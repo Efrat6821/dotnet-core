0f67f94bb7b5de59c9bfa5148f002acfc220cb19
Efrat6821 <Efrat6821+g@gmail.com>
2024-04-01 21:26:16 +0300

    lesson 4 /2

--- Middleware/MyLogMiddleware.cs
+++ Middleware/MyLogMiddleware.cs
@@ -1,28 +1,39 @@
 using System.Diagnostics;
 
-namespace MyMiddleware{
-    
-public class MyLogMiddleware
-    {
-        private readonly RequestDelegate next;
-        private readonly ILogger<MyLogMiddleware> logger;
+// using System.Diagnostics;
+// using System.IO;
+// using Microsoft.AspNetCore.Http;
 
-        public MyLogMiddleware(RequestDelegate next, ILogger<MyLogMiddleware> logger)
+namespace MyMiddleware
+{
+
+    public class MyLogMiddleware
+    {
+        private const string V = "Data/file.txt";
+        private readonly RequestDelegate next;
+        // private readonly ILogger<MyLogMiddleware> logger;
+
+        public MyLogMiddleware(RequestDelegate next/*, ILogger<MyLogMiddleware> logger*/)
         {
             this.next = next;
-            this.logger = logger;
+            // this.logger = logger;
         }
 
         public async Task Invoke(HttpContext c)
         {
-            var act = $"{c.Request.Path}.{c.Request.Method}";
+            string message = $"{c.Request.Path}.{c.Request.Method}";
             var sw = new Stopwatch();
             sw.Start();
-
-            logger.LogInformation($"{act} started at {DateTime.Now:yyyy-MM-dd HH:mm:ss.fff}");
+            message += $" started at {DateTime.Now:yyyy-MM-dd HH:mm:ss.fff}";
+            // logger.LogInformation($"{act} started at {DateTime.Now:yyyy-MM-dd HH:mm:ss.fff}");
             await next.Invoke(c);
-            logger.LogDebug($"{act} ended at {sw.ElapsedMilliseconds} ms."
-            + $" User: {c.User?.FindFirst("userId")?.Value ?? "unknown"}");
+            string path = V;
+            using (StreamWriter writer = File.AppendText(path))
+            {
+                await writer.WriteLineAsync(message);
+            }
+            // logger.LogDebug($"{act} ended at {sw.ElapsedMilliseconds} ms."
+            // + $" User: {c.User?.FindFirst("userId")?.Value ?? "unknown"}");
         }
     }
 
@@ -34,5 +45,6 @@ public class MyLogMiddleware
         }
     }
 }
+
 
 
